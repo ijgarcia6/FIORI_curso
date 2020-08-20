a881a8557fbb174f9f26b857d3ad043318fa427e
ijgarcia6 <68085590+ijgarcia6@users.noreply.github.com>
2020-08-20 15:52:11 -0300

    Update InvoiceList.controller.js

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,18 +1,4 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-<<<<<<< Upstream, based on origin/master
-	"sap/ui/model/json/JSONModel"
-], function (JSONModel, Controller) {
-
-	return Controller.extend("nacho1.UI5_2.controller.InvoiceList", {
-
-       onInit : function () {
-            var oViewModel = new JSONModel({
-                currency: "EUR"
-            });
-            this.getView().setModel(oViewModel, "view");
-        }
-=======
 	"sap/ui/model/json/JSONModel",
 	"../model/formatter",
 	"sap/ui/model/Filter",
@@ -42,7 +28,6 @@ sap.ui.define([
 			oBinding.filter(aFilter);
 			
 		}
-	
->>>>>>> cadf751 commit nuevo
+
 	});
 });
